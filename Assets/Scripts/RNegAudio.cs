8599506229c6b6af3863f0fc1c370e76f131c3cd
lescott-games <lauren.e.scott.games@gmail.com>
2017-10-14 18:41:10 -0700

    More first pass items.
    Negative audio first pass
    Nothing first pass

--- Assets/Scripts/RNegAudio.cs
+++ Assets/Scripts/RNegAudio.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class RNegAudio : MonoBehaviour {
 
+	private AudioSource audioSource;
+
 	// Use this for initialization
 	void Start () {
-		
+		audioSource = gameObject.GetComponent<AudioSource> ();
+		audioSource.Play ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Destroy (gameObject, 2.0f);
+		Destroy (gameObject, 3.0f);
 	}
 }
