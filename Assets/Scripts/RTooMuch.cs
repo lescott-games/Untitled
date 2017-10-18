338528b62c42041d6c24ae399cafa28c7ba70a6b
lescott-games <lauren.e.scott.games@gmail.com>
2017-10-18 12:08:17 -0700

    Minor tweak to RTooMuch.

--- Assets/Scripts/RTooMuch.cs
+++ Assets/Scripts/RTooMuch.cs
@@ -6,13 +6,22 @@ public class RTooMuch : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		Destroy (gameObject, 3.0f);
+		//Destroy (gameObject, 3.0f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+		if ((Input.touchCount == 0 && Input.GetMouseButton (0) == false
+		    && Input.GetMouseButton (1) == false)) {
+			Destroy (gameObject, 0.5f);
+		}
 	}
+
+	void OnMouseUp() {
+		print ("Mouse up.");
+		Destroy (gameObject, 0.5f);
+	}
+
 /*
 	void OnMouseDown() {
 		CancelInvoke ("DestroyMe");
