ae3dd6d0385f2df3675790bcdbf5aa11cc08cc69
lescott-games <lauren.e.scott.games@gmail.com>
2017-12-24 23:48:11 -0800

    First attempt at re-working touch logic.
    Multi-touch object follow.

---
+++ Assets/FingerCircle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Spine;
+using Spine.Unity;
+
+public class FingerCircle : MonoBehaviour {
+
+	public int ID;
+
+	private SkeletonAnimation skeletonAnimation;
+
+	// Use this for initialization
+	void Start () {
+		skeletonAnimation = GetComponent<SkeletonAnimation> ();
+		skeletonAnimation.state.SetAnimation (0, "idle", true);
+		gameObject.GetComponent<MeshRenderer> ().enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/TouchReactTest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchReactTest : MonoBehaviour {
+
+	public FingerCircle[] fingerCircleList;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int fingerCount = 0;
+		foreach (Touch touch in Input.touches) {
+			int modifiedFingerID = touch.fingerId + 1;
+			if (touch.phase != TouchPhase.Ended && touch.phase != TouchPhase.Canceled) {
+				fingerCount++;
+				Vector3 sp = Camera.main.ScreenToWorldPoint (new Vector3 (touch.position.x, touch.position.y, 9));
+				if (fingerCircleList [modifiedFingerID] != null) {
+					fingerCircleList [modifiedFingerID].transform.position = sp;
+					fingerCircleList [modifiedFingerID].GetComponent<MeshRenderer> ().enabled = true;
+				}
+				print ("Modified fingerID: " + modifiedFingerID + ", Transform position: " + fingerCircleList [modifiedFingerID].transform.position);
+			} else {
+				fingerCircleList [modifiedFingerID].GetComponent<MeshRenderer> ().enabled = false;
+			}
+
+		}
+		if (fingerCount > 0)
+			print("User has " + fingerCount + " finger(s) touching the screen");
+	}
+		
+}
